d92ce5f3bb821a83757b8b9851b3740fe493d9be
zaikadm <zaikadm@dtechs.io>
2023-04-12 12:42:28 +0300

    bonus Price commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+
+    public int calculate(int amount) {
+        int miles = amount / 20;
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 13676;
-        int bonusPrice = 20;
-        int bonus = amount / bonusPrice;
-
-        System.out.println("Ваш бонус равен = " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
